ad52d4946a4bd807f2ab0f73dbde51babff73799
Jerson <solorsanolopez@gmail.com>
2022-03-24 19:30:18 -0700

    [Change] people_and_products file names

--- src/shared/utils/products.js
+++ src/shared/utils/products.js
@@ -1,24 +1,3 @@
-export const people = [
-  {
-    name: 'John Doe',
-    age: 54,
-    hairColor: 'brown',
-    hobbies: ['swimming', 'bicycling', 'video games'],
-  },
-  {
-    name: 'Brenda Smith',
-    age: 33,
-    hairColor: 'black',
-    hobbies: ['golf', 'mathematics'],
-  },
-  {
-    name: 'Jane Garcia',
-    age: 27,
-    hairColor: 'blonde',
-    hobbies: ['biology', 'medicine', 'gymnastics'],
-  },
-];
-
 export const products = [
   {
     name: 'Flat-Screen TV',
